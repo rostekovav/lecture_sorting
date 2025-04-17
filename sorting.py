1e09e062b4361d96582aa25a75fb8c61332cdf17
Vladimira Rostekova <vladimira.rostekova@gmail.com>
2025-04-17 08:42:31 +0200

    v

--- sorting.py
+++ sorting.py
@@ -1,4 +1,6 @@
 import os
+import csv
+
 
 
 def read_data(file_name):
@@ -11,9 +13,21 @@ def read_data(file_name):
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
 
+    with open (file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+
+    return data
 
 def main():
-    pass
+    nactene_sekvence = read_data("numbers.csv")
+    print(nactene_sekvence)
 
 
 if __name__ == '__main__':
